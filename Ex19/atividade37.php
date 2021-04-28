4012d5384d0f5e98cb6bf6a263a2a03212e21fd8
Cassio Almeron <cassioalmeron@gmail.com>
2021-04-28 11:17:43 -0300

    First Commit

---
+++ Ex19/atividade37.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = $_POST['nome'];
+$endereco = $_POST['endereco'];
+$tel = $_POST['telefone'];
+$data = $_POST['data_Nascimento'];
+
+echo "<strong>Nome:</strong> {$nome}<br><strong>Endereço:</strong> {$endereco}<br><strong>Telefone:</strong> {$tel}<br>";
